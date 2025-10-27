ab404a8aa366f87da4df93d8a28d83c3e482d569
EzzedeenKhaled <ezzedeenkhaled7@gmail.com>
2025-10-27 10:42:43 +0200

    env

---
+++ src/environments.ts
@@ -0,0 +1,10 @@
+export const environment  = {
+  firebaseConfig: {
+  apiKey: "AIzaSyAe5KFsjSdaddhIglXbUCeMoCsXfp7-EpA",
+  authDomain: "assessment-b1267.firebaseapp.com",
+  projectId: "assessment-b1267",
+  storageBucket: "assessment-b1267.firebasestorage.app",
+  messagingSenderId: "205784902247",
+  appId: "1:205784902247:web:41f2e24055b475c56fc2c2",
+  }
+};
